6046b54bfb1f43beb719253bef89ce775d45eb00
Eton <pk2p007@hotmail.com.tw>
2022-12-24 00:19:47 +0800

    升級 gradle 及支援現行開發環境

--- app/src/main/java/com/example/xposedtest/MainActivity.java
+++ app/src/main/java/com/example/xposedtest/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.xposedtest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
